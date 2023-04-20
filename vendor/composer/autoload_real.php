1c70c7208820c93956fdd79cf12033e8e369fb21
artemka13 <kalabin2004@inbox.ru>
2023-04-20 13:39:33 +0700

    Registration and authentication functions

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit62633ec9bb76413045811569e6133553
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit62633ec9bb76413045811569e6133553::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire62633ec9bb76413045811569e6133553($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire62633ec9bb76413045811569e6133553($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
